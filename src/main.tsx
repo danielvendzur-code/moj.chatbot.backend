a52f381cfc84a9d6b8154b00514c87ae7f0a27b9
danielvendzur-code <daniel.vendzur@gmail.com>
2026-07-20 22:51:41 +0200

    feat: zapni natívne drag railov widgetu

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { getInitialEmbedViewport, isEmbedMode } from "./lib/embedBridge";
+import { installWidgetRailDrag } from "./lib/widgetRailDrag";
 import "./widget.css";
 
 if (isEmbedMode()) {
   document.documentElement.dataset.embed = "true";
   document.documentElement.dataset.embedViewport = getInitialEmbedViewport();
 }
+
+installWidgetRailDrag();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
