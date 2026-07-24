c18557d832e4fdd60cb0ed519b1e85f836e29593
danielvendzur-code <daniel.vendzur@gmail.com>
2026-07-24 16:26:10 +0200

    Enable compact auto-advance in chatbot demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installConfiguratorAutoAdvance } from "./lib/configuratorAutoAdvance";
 import { getInitialEmbedViewport, isEmbedMode } from "./lib/embedBridge";
 import "./widget.css";
 import "./assistant-redesign.css";
 import "./approved-submit-final.css";
 import "./final-user-correction.css";
+
+installConfiguratorAutoAdvance();
 
 if (isEmbedMode()) {
   document.documentElement.dataset.embed = "true";
