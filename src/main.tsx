2d38c2ac9001a17e0e8b4240a3d1cebca0b2b86a
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-04 17:11:08 +0200

    Install the refinement through the runtime style layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { installConfiguratorAutoAdvance } from "./lib/configuratorAutoAdvance";
 import { getInitialEmbedViewport, isEmbedMode } from "./lib/embedBridge";
+import { installProductRefinement } from "./lib/installProductRefinement";
 import "./preview.css";
 import "./widget.css";
 import "./product-widget.css";
-import "./product-refinement.css";
 
 installConfiguratorAutoAdvance();
+installProductRefinement();
 
 if (isEmbedMode()) {
   document.documentElement.dataset.embed = "true";
