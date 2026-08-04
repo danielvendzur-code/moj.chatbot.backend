5d3694af125fcf5cd6f630bbca4972a06dd53e43
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-04 18:38:20 +0200

    Remove the conflicting runtime refinement layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { installConfiguratorAutoAdvance } from "./lib/configuratorAutoAdvance";
 import { getInitialEmbedViewport, isEmbedMode } from "./lib/embedBridge";
-import { installProductRefinement } from "./lib/installProductRefinement";
 import "./preview.css";
 import "./widget.css";
 import "./product-widget.css";
 
 installConfiguratorAutoAdvance();
-installProductRefinement();
 
 if (isEmbedMode()) {
   document.documentElement.dataset.embed = "true";
