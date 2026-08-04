2ccc255512f4262454124a6a1d3a09537e59220d
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-04 12:45:40 +0200

    Load one authoritative widget design system

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { installConfiguratorAutoAdvance } from "./lib/configuratorAutoAdvance";
 import { getInitialEmbedViewport, isEmbedMode } from "./lib/embedBridge";
-import { installLimeWhiteStyles } from "./lib/installLimeWhiteStyles";
-import { installPremiumTilt } from "./lib/premiumTilt";
 import "./preview.css";
 import "./widget.css";
-import "./assistant-redesign.css";
-import "./approved-submit-final.css";
-import "./final-user-correction.css";
-import "./green-motion-final.css";
-import "./unified-experience-final.css";
-import "./masterpiece-final.css";
+import "./product-widget.css";
 
-installLimeWhiteStyles();
 installConfiguratorAutoAdvance();
-installPremiumTilt();
 
 if (isEmbedMode()) {
   document.documentElement.dataset.embed = "true";
