9f19e582f08979334604b8d08ae075a23f5ba768
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-06 11:14:45 +0200

    style: load final liquid glass layer

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./preview.css";
 import "./widget.css";
 import "./product-widget.css";
 import "./widget-polish.css";
+import "./liquid-glass-final.css";
 
 installConfiguratorAutoAdvance();
 
