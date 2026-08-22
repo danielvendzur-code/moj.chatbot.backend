c2287019df67a74229352f74f98a65b6b2ae29b9
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-22 21:55:57 +0200

    Load website-matched widget design system

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import "./dark-chip-final.css";
 import "./logo-match-final.css";
 import "./goal-lock-final.css";
 import "./interaction-stability-final.css";
+import "./website-match-final.css";
+import "./website-match-kage-final.css";
 
 export default function App(): JSX.Element {
   const embedMode = isEmbedMode();
