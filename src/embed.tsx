b0935401fabd3fc5782019d26de03a406084feed
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-31 07:17:06 +0200

    Load restored launcher authority in production embed

--- src/embed.tsx
+++ src/embed.tsx
@@ -5,6 +5,7 @@ import { installConfiguratorAutoAdvance } from "./lib/configuratorAutoAdvance";
 import "./widget.css";
 import "./product-widget.css";
 import "./widget-polish.css";
+import "./logo-match-final.css";
 import "./launch-ready-styles";
 
 installConfiguratorAutoAdvance();
